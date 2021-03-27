87e42bc769b9e48c041e45e976216770ea0db092
theyak <yakkyjunk@hotmail.com>
2021-03-26 22:07:44 -0700

    Update and rename 02-AddTwoNumbers.php to 02-add-two-numbers.php

--- solutions/page1/02-add-two-numbers.php
+++ solutions/page1/02-add-two-numbers.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * 2. Add Two Numbers
+ * URL: https://leetcode.com/problems/add-two-numbers/
+ * Level: Medium (Though so is multiply strings which is much harder)
+
  * You are given two non-empty linked lists representing two non-negative integers. The digits are
  * stored in reverse order, and each of their nodes contains a single digit. Add the two numbers 
  * and return the sum as a linked list.
